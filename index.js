7d6b797044f8cdf96f060995021803db89319725
Jakku Prashanth Reddy <67828370+PrashanthReddy03@users.noreply.github.com>
2022-09-22 21:15:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+//Dice app
+var randomNumber1 = getRandomNumber();
+var randomNumber2 = getRandomNumber();
+
+if (randomNumber1 > randomNumber2)
+{
+    var text = document.querySelectorAll("h1")[0];
+    text.innerHTML = "🚩 Player 1 Wins";
+}
+else
+{
+    var text = document.querySelectorAll("h1")[0];
+    text.innerHTML = "Player 2 Wins 🚩";
+}
+
+var Image1 = randomDiceImage(randomNumber1);
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", Image1);
+
+var Image2 = randomDiceImage(randomNumber2);
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", Image2);
+
+
+function getRandomNumber() {
+  var num =  Math.random();
+    return Math.ceil(num*6);
+}
+
+function randomDiceImage(num) {
+    var Image = "/Images/dice" + num + ".png";
+    return Image;
+}
+
+function refreshPage()
+{
+    window.location.reload();
+}
